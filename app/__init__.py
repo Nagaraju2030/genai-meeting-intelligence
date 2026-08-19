6ad25d29193644e5aa779f2236edb560d7885130
NagaRaju <rajumlai1993@gmail.com>
2026-08-19 14:02:03 +0530

    Add app package initialization

---
+++ app/__init__.py
@@ -0,0 +1,5 @@
+"""GenAI Meeting Intelligence package."""
+
+__version__ = "1.0.0"
+__author__ = "Nagaraju2030"
+__description__ = "Generative AI application that converts meeting transcripts into structured summaries"
